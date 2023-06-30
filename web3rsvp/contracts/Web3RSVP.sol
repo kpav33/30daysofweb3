224eebe10bbb6137583221589e951ef5e58e4a3c
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-06-30 12:29:23 +0200

    Finish up the smart contract

--- web3rsvp/contracts/Web3RSVP.sol
+++ web3rsvp/contracts/Web3RSVP.sol
@@ -3,6 +3,23 @@
 pragma solidity ^0.8.4;
 
 contract Web3RSVP {
+    // Solidity events are a way for our subgraph to listen for specific actions to enable us to make queries about the data from our smart contract
+    // Defining events is adding them as a tool on your toolbelt, but emitting them is actually pulling that tool out and using it
+    event NewEventCreated(
+        bytes32 eventID,
+        address creatorAddress,
+        uint256 eventTimestamp,
+        uint256 maxCapacity,
+        uint256 deposit,
+        string eventDataCID
+    );
+
+    event NewRSVP(bytes32 eventID, address attendeeAddress);
+
+    event ConfirmedAttendee(bytes32 eventID, address attendeeAddress);
+
+    event DepositsPaidOut(bytes32 eventID);
+
     // Struct is similar to a JS object in that it stores related information about an entity
     // Because it is expensive to store data directly on the blockchain, you should only store data that is really necessary directly on the blockchain
     struct CreateEvent {
@@ -21,4 +38,185 @@ contract Web3RSVP {
     // In this example we define a relationship with a unique eventID to the struct that we defined above that is associated with that event
     // We will use this mapping to make sure we are referencing the correct event when we call functions on that event
     mapping(bytes32 => CreateEvent) public idToEvent;
+
+    // Setter function to creat an event
+    // Function visibility set to external, because that will make it more performant and save on gas
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        // Reference to the IPFS hash containing information like the event name and description
+        string calldata eventDataCID
+    ) external {
+        // generate an eventID based on other things passed in to generate a hash
+        // Be careful about collisions when creating hash tables or mappings, collisions occure when two or more elements are referenced by the same hash (unique ID)
+        // For example if two users created an event at the exacatly the same time they might get the same identifier, which could cause issues later when trying to look up elements by their unique eventId
+        // To combat this we generate an ID by creating a hash by passing in all of the arguments of the function, this way we vastly decrease the likelihood of a collision
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+
+        // // To prevent a collision we could also add a require statement that would check that the eventId is not already in use
+        // // make sure this id isn't already claimed
+        // require(idToEvent[eventId].eventTimestamp == 0, "ALREADY REGISTERED");
+
+        // We define the two arrays that we already mentioned in our struct
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+        // this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        // Key is the eventId and the value is a struct with the necessary properties, which we either pass in from the user on the front end and others we generate ourselves or gather from the smart contract side
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+
+        emit NewEventCreated(
+            eventId,
+            msg.sender,
+            eventTimestamp,
+            maxCapacity,
+            deposit,
+            eventDataCID
+        );
+    }
+
+    // Function that gets called when a user finds an event and RSVPs to it on the front end
+    function createNewRSVP(bytes32 eventId) external payable {
+        // look up event from our mapping
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        // transfer deposit to our contract / require that they send in enough ETH to cover the deposit requirement of this specific event
+        require(msg.value == myEvent.deposit, "NOT ENOUGH");
+
+        // require that the event hasn't already happened (<eventTimestamp)
+        require(block.timestamp <= myEvent.eventTimestamp, "ALREADY HAPPENED");
+
+        // make sure event is under max capacity
+        require(
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
+            "This event has reached capacity"
+        );
+
+        // require that msg.sender isn't already in myEvent.confirmedRSVPs AKA hasn't already RSVP'd
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(
+                myEvent.confirmedRSVPs[i] != msg.sender,
+                "ALREADY CONFIRMED"
+            );
+        }
+
+        myEvent.confirmedRSVPs.push(payable(msg.sender));
+
+        emit NewRSVP(eventId, msg.sender);
+    }
+
+    // Function that checks in attendees and returns their deposit
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        // look up event from our struct using the eventId
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        // require that msg.sender is the owner of the event - only the host should be able to check people in
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        // require that attendee trying to check in actually RSVP'd
+        address rsvpConfirm;
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            if (myEvent.confirmedRSVPs[i] == attendee) {
+                rsvpConfirm = myEvent.confirmedRSVPs[i];
+            }
+        }
+
+        require(rsvpConfirm == attendee, "NO RSVP TO CONFIRM");
+
+        // require that attendee is NOT already in the claimedRSVPs list AKA make sure they haven't already checked in
+        for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != attendee, "ALREADY CLAIMED");
+        }
+
+        // require that deposits are not already claimed by the event owner
+        require(myEvent.paidOut == false, "ALREADY PAID OUT");
+
+        // add the attendee to the claimedRSVPs list
+        myEvent.claimedRSVPs.push(attendee);
+
+        // sending eth back to the staker `https://solidity-by-example.org/sending-ether`
+        (bool sent, ) = attendee.call{value: myEvent.deposit}("");
+
+        // if this fails, remove the user from the array of claimed RSVPs
+        if (!sent) {
+            myEvent.claimedRSVPs.pop();
+        }
+
+        require(sent, "Failed to send Ether");
+
+        emit ConfirmedAttendee(eventId, attendee);
+    }
+
+    // Function to confirm every person that has RSVPs to a specific event
+    function confirmAllAttendees(bytes32 eventId) external {
+        // look up event from our struct with the eventId
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        // make sure you require that msg.sender is the owner of the event
+        require(msg.sender == myEvent.eventOwner, "NOT AUTHORIZED");
+
+        // confirm each attendee in the rsvp array
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    // Function that will withdraw deposits of people who didn’t show up to the event and send them to the event organizer
+    function withdrawUnclaimedDeposits(bytes32 eventId) external {
+        // look up event
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        // check that the paidOut boolean still equals false AKA the money hasn't already been paid out
+        require(!myEvent.paidOut, "ALREADY PAID");
+
+        // check if it's been 7 days past myEvent.eventTimestamp
+        require(
+            block.timestamp >= (myEvent.eventTimestamp + 7 days),
+            "TOO EARLY"
+        );
+
+        // only the event owner can withdraw
+        require(msg.sender == myEvent.eventOwner, "MUST BE EVENT OWNER");
+
+        // calculate how many people didn't claim by comparing
+        uint256 unclaimed = myEvent.confirmedRSVPs.length -
+            myEvent.claimedRSVPs.length;
+
+        uint256 payout = unclaimed * myEvent.deposit;
+
+        // mark as paid before sending to avoid reentrancy attack
+        myEvent.paidOut = true;
+
+        // send the payout to the owner
+        (bool sent, ) = msg.sender.call{value: payout}("");
+
+        // if this fails
+        if (!sent) {
+            myEvent.paidOut = false;
+        }
+
+        require(sent, "Failed to send Ether");
+
+        emit DepositsPaidOut(eventId);
+    }
 }
