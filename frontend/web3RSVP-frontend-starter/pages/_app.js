d35fd4be345e97732cdb1348b3a965254974ce20
Klemen Pavlovič <klemenpavlovic@gmail.com>
2023-07-04 09:40:19 +0200

    Add graph queries

--- frontend/web3RSVP-frontend-starter/pages/_app.js
+++ frontend/web3RSVP-frontend-starter/pages/_app.js
@@ -7,6 +7,9 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
 
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo-client";
+
 // const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
 const { chains, provider } = configureChains(
@@ -32,9 +35,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
+        <ApolloProvider client={client}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ApolloProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
